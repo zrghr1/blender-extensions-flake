fba622cac0353f71b001c459dcf481361fe77649
zrghr <cassiusharju@proton.me>
2025-09-13 01:33:56 +0300

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Blender Extensions Flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ { self, nixpkgs, home-manager, ...  }: {
+    homeModules = import ./src/default.nix;
+  };
+}
