7d6d0300984779a4231ebc92147d729d89944b41
zrghr <cassiusharju@proton.me>
2025-09-13 14:26:54 +0300

    Add io_import_dxf

---
+++ src/extensions/io_import_dxf.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "io_import_dxf";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/io_import_dxf.git";
+    rev = "fe53f891b814d6172eabc33c0aad2f02b1698718";
+    sha256 = "fe53f891b814d6172eabc33c0aad2f02b1698718";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
