57a266371d437474e0954278c94216555f8d920c
zrghr <cassiusharju@proton.me>
2025-09-13 14:02:17 +0300

    Add btrace

---
+++ src/extensions/btrace.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "btrace";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/${name}.git";
+    rev = "65887b999a9a6a153c341120676883fefb76e1d9";
+    sha256 = "65887b999a9a6a153c341120676883fefb76e1d9";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
