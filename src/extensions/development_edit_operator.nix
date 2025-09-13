faaa7e8ba874c85a07dbb78dc42983dff77b95ff
zrghr <cassiusharju@proton.me>
2025-09-13 15:21:26 +0300

    Add development_edit_operator

--- src/extensions/development_edit_operator.nix
+++ src/extensions/development_edit_operator.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/development_edit_operator.git";
     rev = "66795fc4c9b721820d634bbd8ac875a7e2d2eb04";
-    sha256 = "66795fc4c9b721820d634bbd8ac875a7e2d2eb04";
+    sha256 = "1rfchmmgd5qpnhmj4ah8lmbcj4g06mmwfp3alzvgyvv4gb8bykk7";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
