951c417eacbd1460cd5109b15188ab67800a6a58
zrghr <cassiusharju@proton.me>
2025-09-13 13:53:41 +0300

    Add print3d_toolbox

---
+++ src/extensions/print3d_toolbox.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "print3d_toolbox";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/${name}.git";
+    rev = "ec7914d9c5b3222806a54a2bb32ea1619659654f";
+    sha256 = "ec7914d9c5b3222806a54a2bb32ea1619659654f";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
