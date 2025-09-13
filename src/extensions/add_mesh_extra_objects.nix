690fb35a26b5be81b5e49d65e526f2a96faead3e
zrghr <cassiusharju@proton.me>
2025-09-13 15:22:54 +0300

    Add add_mesh_extra_objects

--- src/extensions/add_mesh_extra_objects.nix
+++ src/extensions/add_mesh_extra_objects.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/add_mesh_extra_objects.git";
     rev = "b4f555b7e7d20168eb01c7daf69e45765a59c381";
-    sha256 = "b4f555b7e7d20168eb01c7daf69e45765a59c381";
+    sha256 = "1j9cw56ipl0wdcny1p9nyxd6l2dx43iq81lc2j7f3r0h5ciybwra";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
