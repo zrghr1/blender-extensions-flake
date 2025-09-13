8e01e3edef82b2a1316475e491c1cc8f1bdd4a8b
zrghr <cassiusharju@proton.me>
2025-09-13 14:47:26 +0300

    Add materials_library_vx

---
+++ src/extensions/materials_library_vx.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "materials_library_vx";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/materials_library_vx.git";
+    rev = "4331d6043e431cf0772842b003c04114c9637d8e";
+    sha256 = "4331d6043e431cf0772842b003c04114c9637d8e";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
