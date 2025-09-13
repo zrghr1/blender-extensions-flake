51cc75501373504b2b97f9528e7162f10495a85e
zrghr <cassiusharju@proton.me>
2025-09-13 15:23:40 +0300

    Add curve_simplify

--- src/extensions/curve_simplify.nix
+++ src/extensions/curve_simplify.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/curve_simplify.git";
     rev = "585906a99662630e341d2a66cb0cad3ac716730a";
-    sha256 = "585906a99662630e341d2a66cb0cad3ac716730a";
+    sha256 = "1ifac271arlr08vklx8qf3874f3139gw3vzlpy9lmchq0061s9cd";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
