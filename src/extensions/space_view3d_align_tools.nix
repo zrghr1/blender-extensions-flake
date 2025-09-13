3fc860932fe520f565d64bd08676ca315628bd4c
zrghr <cassiusharju@proton.me>
2025-09-13 14:04:21 +0300

    Add space_view3d_align_tools

---
+++ src/extensions/space_view3d_align_tools.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "space_view3d_align_tools";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/${name}.git";
+    rev = "f911bd84260292f3e578b2df715fceb75141efb4";
+    sha256 = "f911bd84260292f3e578b2df715fceb75141efb4";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
