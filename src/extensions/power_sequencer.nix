fd1885febd30d41ae099ead3c1f6c83a850660b8
zrghr <cassiusharju@proton.me>
2025-09-13 14:23:33 +0300

    Add power_sequencer

---
+++ src/extensions/power_sequencer.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "power_sequencer";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/power_sequencer.git";
+    rev = "d67aeb2c170af7addda99c57e06da06313e5f8c9";
+    sha256 = "d67aeb2c170af7addda99c57e06da06313e5f8c9";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
