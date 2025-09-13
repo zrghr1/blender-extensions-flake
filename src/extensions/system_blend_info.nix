6af1d3bef1ea711a970c6ea460649a3fa86ff6e2
zrghr <cassiusharju@proton.me>
2025-09-13 15:18:08 +0300

    Add system_blend_info

--- src/extensions/system_blend_info.nix
+++ src/extensions/system_blend_info.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/system_blend_info.git";
     rev = "56c39b893543ecd5dac4eb03430466bf30aa1634";
-    sha256 = "56c39b893543ecd5dac4eb03430466bf30aa1634";
+    sha256 = "0a17jjsj6kci3ywaqinsih37lh9npc2gqjfgza1k4q33y8a7g3xr";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
