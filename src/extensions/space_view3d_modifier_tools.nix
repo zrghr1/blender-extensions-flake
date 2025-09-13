03591d478249954d0d9feebe688145a0baa5cda2
zrghr <cassiusharju@proton.me>
2025-09-13 15:24:06 +0300

    Add space_view3d_modifier_tools

--- src/extensions/space_view3d_modifier_tools.nix
+++ src/extensions/space_view3d_modifier_tools.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/space_view3d_modifier_tools.git";
     rev = "2bc411dc6e856f32fac98eae086abfeb5b7306fc";
-    sha256 = "2bc411dc6e856f32fac98eae086abfeb5b7306fc";
+    sha256 = "0i1gjlqspcx8zjcdrj25p2fm2v7n5j3nmgz8mgk8l1vj4nq5bnd5";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
