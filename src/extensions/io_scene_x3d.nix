1eddb784e1b540ae190694dc3ce13afec73cdbf1
zrghr <cassiusharju@proton.me>
2025-09-13 15:29:50 +0300

    Add io_scene_x3d

--- src/extensions/io_scene_x3d.nix
+++ src/extensions/io_scene_x3d.nix
@@ -2,9 +2,9 @@
 let
   name = "io_scene_x3d";
   repo =  pkgs.fetchgit {
-    url = "https://projects.blender.org/extensions/io_scene_3ds.git";
+    url = "https://projects.blender.org/extensions/io_scene_x3d.git";
     rev = "e956bd8beaa6ac33d7e7efb7419f15b9291c4dba";
-    sha256 = "e956bd8beaa6ac33d7e7efb7419f15b9291c4dba";
+    sha256 = "1wxlkcjc7dy06jknfdsc1cfmzvkbzd2xbfl7jisdkfz8k5ifd4q3";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
