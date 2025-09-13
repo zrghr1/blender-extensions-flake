a23e6facc71adafe270ab3fbe41c12df27b7f4de
zrghr <cassiusharju@proton.me>
2025-09-13 14:43:08 +0300

    Add space_view3d_brush_menus

---
+++ src/extensions/space_view3d_brush_menus.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "space_view3d_brush_menus";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/space_view3d_brush_menus.git";
+    rev = "34b28a1d4d9d3764d98ca1b2003ef4c96a39f5d0";
+    sha256 = "34b28a1d4d9d3764d98ca1b2003ef4c96a39f5d0";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
