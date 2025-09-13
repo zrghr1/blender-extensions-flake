d9a33e8b4dfcc4cda52ae20ba6a1f5dc443f3b0a
zrghr <cassiusharju@proton.me>
2025-09-13 14:41:33 +0300

    Add materials_utils

---
+++ src/extensions/materials_utils.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "materials_utils";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/materials_utils.git";
+    rev = "699761cf542a5750d43a06e3608cc03a63bc5cf5";
+    sha256 = "699761cf542a5750d43a06e3608cc03a63bc5cf5";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
