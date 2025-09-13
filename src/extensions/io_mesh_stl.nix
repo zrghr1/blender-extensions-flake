54626575d19c3bfb771c8cebec39f843d6e4f0c9
zrghr <cassiusharju@proton.me>
2025-09-13 15:27:12 +0300

    Add io_mesh_stl

--- src/extensions/io_mesh_stl.nix
+++ src/extensions/io_mesh_stl.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/io_mesh_stl.git";
     rev = "d58d17d7b9fae51c1f37e0188122a7c81034266c";
-    sha256 = "d58d17d7b9fae51c1f37e0188122a7c81034266c";
+    sha256 = "18gcybc6b1x3w837awl2xaxcb3p7lmfpwgwc2ci1nq2npvkfr9bf";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
