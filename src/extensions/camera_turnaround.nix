4695d0bdbc352aa4f6e8366754250c344f719715
zrghr <cassiusharju@proton.me>
2025-09-13 14:45:12 +0300

    Add camera_turnaround

---
+++ src/extensions/camera_turnaround.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "camera_turnaround";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/camera_turnaround.git";
+    rev = "0a0f13b93e6fb9f2cd6838dff842f63c1fcc7c6b";
+    sha256 = "0a0f13b93e6fb9f2cd6838dff842f63c1fcc7c6b";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
