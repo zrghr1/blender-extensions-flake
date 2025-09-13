299034116f520c833c20dcf46e4d2fb2d35c77c1
zrghr <cassiusharju@proton.me>
2025-09-13 14:37:37 +0300

    Add animation_animall

---
+++ src/extensions/animation_animall.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "animation_animall";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/animation_animall.git";
+    rev = "97a8e2a72b28098ffc90e18755f3fa293b7b0042";
+    sha256 = "97a8e2a72b28098ffc90e18755f3fa293b7b0042";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
