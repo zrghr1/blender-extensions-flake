b5493785daec9880a8b82896bcb917edb9dc197e
zrghr <cassiusharju@proton.me>
2025-09-13 15:21:33 +0300

    Add node_wrangler

--- src/extensions/node_wrangler.nix
+++ src/extensions/node_wrangler.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/node_wrangler.git";
     rev = "3697b44db4861f0cf9453e019aecc25673046309";
-    sha256 = "3697b44db4861f0cf9453e019aecc25673046309";
+    sha256 = "0q2a3ykzi13fa7k9ix75mybvvc7c2a16li0nldbrdyd9q5ha1zp1";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
