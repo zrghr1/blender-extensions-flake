51b1ff235141f2145c231b129629f2a42242438f
zrghr <cassiusharju@proton.me>
2025-09-13 15:25:26 +0300

    Add io_shape_mdd

--- src/extensions/io_shape_mdd.nix
+++ src/extensions/io_shape_mdd.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/io_shape_mdd.git";
     rev = "b0d0b02821e66bbe0f89e53e2ec4323f74765249";
-    sha256 = "b0d0b02821e66bbe0f89e53e2ec4323f74765249";
+    sha256 = "0clibxb4nzsgb2wyg8dks50qd3bqhw4g8vyr4d9bznfdn5il5p48";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
