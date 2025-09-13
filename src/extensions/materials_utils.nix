f4c30663b35d2b16c1acf878ac84d61d54116f03
zrghr <cassiusharju@proton.me>
2025-09-13 15:20:54 +0300

    Add materials_utils

--- src/extensions/materials_utils.nix
+++ src/extensions/materials_utils.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/materials_utils.git";
     rev = "699761cf542a5750d43a06e3608cc03a63bc5cf5";
-    sha256 = "699761cf542a5750d43a06e3608cc03a63bc5cf5";
+    sha256 = "0kpjgfxlw442rvbzim3j2vdz98q2vynnbmpgi6fqr7vg50z6nk7c";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
