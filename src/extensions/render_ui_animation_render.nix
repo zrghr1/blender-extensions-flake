d0d02e495f1f6cc6dd82f20f3236e6051c8a8dba
zrghr <cassiusharju@proton.me>
2025-09-13 15:20:05 +0300

    Add render_ui_animation_render

--- src/extensions/render_ui_animation_render.nix
+++ src/extensions/render_ui_animation_render.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/render_ui_animation_render.git";
     rev = "06e9e85e9505be679feb1cbc3747eafbfe05f764";
-    sha256 = "06e9e85e9505be679feb1cbc3747eafbfe05f764";
+    sha256 = "1k5lw9d3zhgnwix3vfaysbrgqx9w8w3qw5l0qcds8hx1grnrxap1";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
