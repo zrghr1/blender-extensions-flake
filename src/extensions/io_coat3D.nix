b6ee576330e3223c4561d04fe431f38c89fea266
zrghr <cassiusharju@proton.me>
2025-09-13 15:21:54 +0300

    Add io_coat3D

--- src/extensions/io_coat3D.nix
+++ src/extensions/io_coat3D.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/io_coat3D.git";
     rev = "8b39ba497d29bc961d6eab3beb21d384ec48fd2a";
-    sha256 = "8b39ba497d29bc961d6eab3beb21d384ec48fd2a";
+    sha256 = "1clfpqii2csn77417ffar0ir9gsyfkmab9fn9sjfi4nxhpmpnpp1";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
