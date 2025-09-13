982071de46c4c5a8e36fa3406652c66c246b4720
zrghr <cassiusharju@proton.me>
2025-09-13 15:19:19 +0300

    Add object_skinify

--- src/extensions/object_skinify.nix
+++ src/extensions/object_skinify.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/object_skinify.git";
     rev = "0fe31e02ea9d86f70fee9a9cf97b719af0454b0e";
-    sha256 = "0fe31e02ea9d86f70fee9a9cf97b719af0454b0e";
+    sha256 = "127iv2jli4c66ysq1g66nizm61h7vpsgrng6y3aqky52mh2fbnh4";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
