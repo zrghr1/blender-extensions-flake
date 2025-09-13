4786da498d4a0149ba2c58a8dfea38b36679cbf2
zrghr <cassiusharju@proton.me>
2025-09-13 14:43:29 +0300

    Add space_view3d_math_vis

---
+++ src/extensions/space_view3d_math_vis.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "space_view3d_math_vis";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/space_view3d_math_vis.git";
+    rev = "cd81f3d09ba6fa4d446a11afeb4a63ae7a4917f8";
+    sha256 = "cd81f3d09ba6fa4d446a11afeb4a63ae7a4917f8";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
