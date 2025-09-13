a0812236754eee4caa06b932dec77f073826ed28
zrghr <cassiusharju@proton.me>
2025-09-13 14:28:05 +0300

    Add io_import_BrushSet

---
+++ src/extensions/io_import_BrushSet.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "io_import_BrushSet";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/io_import_BrushSet.git";
+    rev = "f4e9d97039c688e09fc9137c664321512d400b45";
+    sha256 = "f4e9d97039c688e09fc9137c664321512d400b45";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
