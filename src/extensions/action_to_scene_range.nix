6cde17161f923c8065857446a9172d1226f0be30
zrghr <cassiusharju@proton.me>
2025-09-13 14:36:36 +0300

    Add action_to_scene_range

---
+++ src/extensions/action_to_scene_range.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "action_to_scene_range";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/action_to_scene_range.git";
+    rev = "f638503e4a6a9a9d58f247aae1617f5ce0b24fcf";
+    sha256 = "f638503e4a6a9a9d58f247aae1617f5ce0b24fcf";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
