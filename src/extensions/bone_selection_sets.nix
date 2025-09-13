a694432d8a6cf3beb49daa12e06ac5203f621a73
zrghr <cassiusharju@proton.me>
2025-09-13 14:01:28 +0300

    Add bone_selection_sets

---
+++ src/extensions/bone_selection_sets.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "bone_selection_sets";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/${name}.git";
+    rev = "27b42e9aae0901c758275967ac8a92bff39e950f";
+    sha256 = "27b42e9aae0901c758275967ac8a92bff39e950f";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
