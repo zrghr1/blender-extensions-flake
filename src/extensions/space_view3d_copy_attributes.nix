d885fb756c1c955d3e974657f5e7bcdc8c158088
zrghr <cassiusharju@proton.me>
2025-09-13 15:22:45 +0300

    Add space_view3d_copy_attributes

--- src/extensions/space_view3d_copy_attributes.nix
+++ src/extensions/space_view3d_copy_attributes.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/space_view3d_copy_attributes.git";
     rev = "5edd30f73091492b02c006cf883fff9c0d37c19a";
-    sha256 = "5edd30f73091492b02c006cf883fff9c0d37c19a";
+    sha256 = "1fggp3pnd5d14vfwzw6wgl2wzj3xf9szhjp36fzjzp2xsadb55my";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
