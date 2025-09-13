87d524de281d4348c2d38638e419ba6c6761555c
zrghr <cassiusharju@proton.me>
2025-09-13 15:26:06 +0300

    Add io_export_paper_model

--- src/extensions/io_export_paper_model.nix
+++ src/extensions/io_export_paper_model.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/io_export_paper_model.git";
     rev = "fb13f8ed5b55472e06b5e20e8299059ae6363b51";
-    sha256 = "fb13f8ed5b55472e06b5e20e8299059ae6363b51";
+    sha256 = "1n7pyxkgha5yqqnqxy1bpp5j8lslngmfmk4d12d48lm0phcxgyr5";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
