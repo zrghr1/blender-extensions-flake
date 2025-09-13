ca47795f53857fecf4ca3f0a866aaab35cec3442
zrghr <cassiusharju@proton.me>
2025-09-13 15:29:18 +0300

    Add precision_drawing_tools

--- src/extensions/precision_drawing_tools.nix
+++ src/extensions/precision_drawing_tools.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/precision_drawing_tools.git";
     rev = "4f7eaa2b3b06dafb5b321cd1904608964d4eac51";
-    sha256 = "4f7eaa2b3b06dafb5b321cd1904608964d4eac51";
+    sha256 = "1r68jfxqbfy7q4946z2fcfn9r987knjzslirnby68c44fbpqn91y";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
