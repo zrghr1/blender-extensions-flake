0660c7ae3284009319d1399bed14f2db81d0530f
zrghr <cassiusharju@proton.me>
2025-09-13 15:21:47 +0300

    Add vdm_brush_baker

--- src/extensions/vdm_brush_baker.nix
+++ src/extensions/vdm_brush_baker.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/vdm_brush_baker.git";
     rev = "8f96c642d26334fb1813200524a59ec9856ff5a2";
-    sha256 = "8f96c642d26334fb1813200524a59ec9856ff5a2";
+    sha256 = "17f3dalv1k2gwyy2h21ifwm58ii6ib8m6yaygp33g39d3y5zqdzx";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
