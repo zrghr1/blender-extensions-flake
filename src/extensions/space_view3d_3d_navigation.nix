344a446b5a07c9fa8b7b98e609a10b4cfa20f18a
zrghr <cassiusharju@proton.me>
2025-09-13 15:27:22 +0300

    Add space_view3d_3d_navigation

--- src/extensions/space_view3d_3d_navigation.nix
+++ src/extensions/space_view3d_3d_navigation.nix
@@ -4,7 +4,7 @@ let
   repo =  pkgs.fetchgit {
     url = "https://projects.blender.org/extensions/space_view3d_3d_navigation.git";
     rev = "8917d5a8064960cef71797d97c1101b3ae5c642f";
-    sha256 = "8917d5a8064960cef71797d97c1101b3ae5c642f";
+    sha256 = "03mdv9gkvzs4imn3g7q520ck7a0s06s4g24pzcki561acc37g6a5";
   };
   package = pkgs.runCommand "blender-extension-${name}" {} ''
     mkdir -p $out
