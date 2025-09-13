286fc21b267da7d1ea1ad1147373cd5cf6d76a34
zrghr <cassiusharju@proton.me>
2025-09-13 14:35:23 +0300

    Add space_view3d_copy_attributes

---
+++ src/extensions/space_view3d_copy_attributes.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs }:
+let
+  name = "space_view3d_copy_attributes";
+  repo =  pkgs.fetchgit {
+    url = "https://projects.blender.org/extensions/space_view3d_copy_attributes.git";
+    rev = "5edd30f73091492b02c006cf883fff9c0d37c19a";
+    sha256 = "5edd30f73091492b02c006cf883fff9c0d37c19a";
+  };
+  package = pkgs.runCommand "blender-extension-${name}" {} ''
+    mkdir -p $out
+    cp -r ${repo}/source $out/${name}
+  '';
+in
+{
+  inherit name package;
+  toPython = ''
+    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.${name}")
+  '';
+}
